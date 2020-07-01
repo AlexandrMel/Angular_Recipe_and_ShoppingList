9034e74ced850236c3cbc8c9e06299bb81ba327a
AlexandrMel <melnic.alexandr88@gmail.com>
2020-07-01 22:18:10 +0200

    Added navbar and started shaping recipe-list component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+recipes: Recipe[] = [
+  new Recipe('A Test Recipe', 'This is a test ', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/healthy-dinner-ideas-tofu-bowl-1574613204.jpg?crop=0.811xw:0.541xh;0.0737xw,0.247xh&resize=640:*')
+]
   constructor() { }
 
   ngOnInit(): void {
